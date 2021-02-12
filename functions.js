83e40186fd205f1eed334866caba04abd89e6f0e
Kirsi Ralko <72715839+kirsralk@users.noreply.github.com>
2021-02-12 15:28:06 -0600

    updated funcion.js to connect to mysql

--- functions.js
+++ functions.js
@@ -1,4 +1,20 @@
+var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "empTrack_DB"
+});
 
 class Functions {
     constructor() {
@@ -7,33 +23,46 @@ class Functions {
 
     addData() {
         inquirer
-         .prompt({
-            name: "action",
-            type: "rawlist",
-            message: "Welcome to Employee Tracker. What would you like to do?",
-            choices: [
-                "Add a department",
-                "Add a role",
-                "Add an employee"
-            ]
-        })
+         .prompt([
+            {
+                name: "action",
+                type: "rawlist",
+                message: "Welcome to Employee Tracker. What would you like to do?",
+                choices: [
+                    "Add a department",
+                    "Add a role",
+                    "Add an employee"
+                ]},
+            {
+                name: "dept",
+                type: "input",
+                message: "Enter the new department name: "
+            }
+        ])
         .then(function(answer) {
             switch (answer.action) {
-            case "Add departments, roles, employees":
-                addData();
+            case "Add a department":
+                // console.log("this line ran");
+                connection.query(
+                    "INSERT INTO department SET ?",
+                    {
+                        name: answer.dept
+                    }
+                )
                 break;
             
-            case "View departments, roles, employees":
+            case "Add a role":
                 viewData();
                 break;
             
-            case "Update employee roles":
+            case "Add an employee":
                 updateData();
                 break;
             } 
 
         }); // end .then function
     } //end start() function
+
 
 } // end class
 
